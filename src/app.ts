8e1881be83d30d49a7f770d10c310820686654ed
NihalDew <nihaldeew1998@gmail.com>
2021-02-22 12:08:16 +0530

    new day"

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { request, response } from 'express';
-var nodemailer = require("nodemailer");
-// const mailchimpClient = require("@mailchimp/mailchimp_transactional")("'fSTbY9Q5pNCqykcitjBqzw");
-var mandrillTransport = require('nodemailer-mandrill-transport');
+// var nodemailer = require("nodemailer");
+const mailchimpClient = require("@mailchimp/mailchimp_transactional")("fSTbY9Q5pNCqykcitjBqzw");
+// var mandrillTransport = require('nodemailer-mandrill-transport');
 
 // ----------------------------------------
 // var message = {
@@ -15,11 +15,11 @@ var mandrillTransport = require('nodemailer-mandrill-transport');
 
 
 
-var smtpTransport = nodemailer.createTransport(mandrillTransport({
-    auth: {
-      apiKey : 'fSTbY9Q5pNCqykcitjBqzw'
-    }
-}));
+// var smtpTransport = nodemailer.createTransport(mandrillTransport({
+//     auth: {
+//       apiKey : 'fSTbY9Q5pNCqykcitjBqzw'
+//     }
+// }));
 
 
 const app = express();
@@ -40,20 +40,20 @@ app.post('/pp', (req, res) => {
 
     
 
-    let mailData={
-          from : req.body.from_email,
-          to : req.body.to["email"],
-          subject : req.body.subject,
-          html : req.body.html
-       };
+    // let mailData={
+    //       from : req.body.from_email,
+    //       to : req.body.to["email"],
+    //       subject : req.body.subject,
+    //       html : req.body.html
+    //    };
       
-       smtpTransport.sendMail(mailData, function(error: any, response: any){
-          if(error) {
-             throw new Error("Error in sending email");
-          }
-          console.log("Message sent: " + JSON.stringify(response));
-          // console.log(req.body);
-        });
+    //    smtpTransport.sendMail(mailData, function(error: any, response: any){
+    //       if(error) {
+    //          throw new Error("Error in sending email");
+    //       }
+    //       console.log("Message sent: " + JSON.stringify(response));
+    //       // console.log(req.body);
+    //     });
 
         //  message = {
         //     from : req.body.from_email,
@@ -62,22 +62,22 @@ app.post('/pp', (req, res) => {
         //     html : req.body.html
         //  };
 
-            // const run = async () => {
-            // // const api_res = await mailchimpClient.messages.send({ message: message });
-            // const api_res = await mailchimpClient.messages.send({ message: {"from_email":"nihaldewangan487@gmail.com", 
-            //     "subject":"Hello World",
-            //     "text":"Welcome to Mailchimp Transactional!",
-            //     "to":[
-            //         {
-            //          "email":"nihaldewangan487@gmail.com",
-            //          "type":"to"
-            //             }
-            //           ] 
-            //            } });
-            //     console.log(api_res);
-            //   };
+            const run = async () => {
+            // const api_res = await mailchimpClient.messages.send({ message: message });
+            const api_res = await mailchimpClient.messages.send({ message: {"from_email":"nihaldewangan487@gmail.com", 
+                "subject":"Hello World",
+                "text":"Welcome to Mailchimp Transactional!",
+                "to":[
+                    {
+                     "email":"nihaldewangan487@gmail.com",
+                     "type":"to"
+                        }
+                      ] 
+                       } });
+                console.log(api_res);
+              };
               
-            //   run()
+              run()
             res.send("assa");
 
         });
